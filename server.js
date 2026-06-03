b3ac5e4f733c0843d10bd893539f6f3c274f6dc3
maruf <marufahmed7800@gmail.com>
2026-06-04 00:41:08 +0600

    Fix: Clear server port bindings and strip hardcoded OS root drive dependencies also frontend link connected

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 
 // --- 🛡️ INITIALIZE GLOBAL MIDDLEWARE (DECLARED ONCE) ---
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://127.0.0.1:5173',"https://pocket-sculpt-frontend.vercel.app/"],
+    origin: ['http://localhost:5173', 'http://127.0.0.1:5173',"https://pocket-sculpt-frontend.vercel.app"],
     credentials: true
 }));
 app.use(express.json());
